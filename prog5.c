90fbdd9e3231e8b0e3196cf4b4ed66c91da9253d
Josh Lindoo <joshua.lindoo@gmail.com>
2015-02-24 14:41:22 -0500

    init commit

---
+++ prog5.c
@@ -0,0 +1,24 @@
+/*****************************************************************************/
+/* Josh Lindoo
+/* Login ID: lind6441
+/* CS-202, Winter 2015
+/* Programming Assignment 5
+/* Ticker Tape Display - Scrolls file contents across terminal
+/*****************************************************************************/
+
+//includes
+
+
+//global declarations
+
+
+/*****************************************************************************/
+/* Function: main
+/* Purpose: 
+/* Parameters: int argc, char* argv[]
+/* Returns: int
+/*****************************************************************************/
+int main( int argc, char* argv[] )
+{
+    return 0;
+}
