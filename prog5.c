d054d82c68a0df935b2098a57dc205bfc22ed9a7
Josh Lindoo <joshua.lindoo@gmail.com>
2015-02-26 23:23:40 -0500

    pushing actual code

--- prog5.c
+++ prog5.c
@@ -8,6 +8,7 @@
 
 //includes
 #include <stdio.h>
+#include <stdlib.h>
 #include <signal.h>
 #include <sys/time.h>
 #include <curses.h>
@@ -15,16 +16,50 @@
 //global declarations
 int row, col = 0; //current row, col
 int numRows, numCols; //number of rows, cols
-int msDelay = 1000; //default delay between scrolling
+int maxCharsToPrint = 1; //max number of characters to print to window
+int leftTruncate = 0; //chars to truncate from left side of the screen
+int msDelay = 100; //default delay between scrolling
+char *fileBuffer; //file buffer to be dynamically allocated later
+long fileSize; //size of input file
+
+//global options
+int loopMode = 0;
 
 
 void scrollMessage(int signum)
 {
     signal(SIGALRM, scrollMessage); //reset signal binding jic
 
-    move(row,col);
-    addstr("X");
-    col++;
+    mvprintw(row, col-1, "%.*s", maxCharsToPrint, fileBuffer + leftTruncate);
+
+    if(col-1 != 0)
+    {
+        col--;
+    }
+    else if(leftTruncate < fileSize)
+    {
+        leftTruncate++;
+    }
+
+    if(maxCharsToPrint < numCols) maxCharsToPrint++;
+
+    if(leftTruncate == fileSize)
+    {
+        if(loopMode)
+        {
+            //reset everything
+            col = numCols;
+            leftTruncate = 0;
+            maxCharsToPrint = 1;
+        }
+        else
+        {
+            endwin();
+            exit(0);
+        }
+    }
+
+    mvprintw(1,0, "col-1 = %d, leftTruncate = %d, fileSize = %d, maxCharsToPrint = %d", col-1, leftTruncate, fileSize, maxCharsToPrint);
     refresh();
 }
 
@@ -53,11 +88,19 @@ int setTicker( int n_msecs )
 /*****************************************************************************/
 int main( int argc, char* argv[] )
 {
-    
+    FILE *file = fopen("test", "r"); //open the file for reading
+    fseek(file, 0, SEEK_END); //seek to end
+    fileSize = ftell(file); //store the end pos as file size
+    rewind(file); //back to the start
+
+    fileBuffer = malloc(fileSize+1);
+    fread(fileBuffer, fileSize, 1, file); //read whole file
+    fclose(file);
+    // printf("file size: %d\n", fsize);
     
     initscr();
-    crmode();
-    noecho();
+    curs_set(0); //no curser
+    // noecho();
     clear();
 
     getmaxyx(stdscr, numRows, numCols); //can only use after initscr()
@@ -75,8 +118,6 @@ int main( int argc, char* argv[] )
             pause();
         }
     }
-
-    endwin();
 
     return 0;
 }
