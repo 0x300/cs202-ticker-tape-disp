53c1dd43fb72c1120dc0959663e3e31b0735ee37
Josh Lindoo <joshua.lindoo@gmail.com>
2015-02-26 21:20:07 -0500

    alarm and printing to window working

--- prog5.c
+++ prog5.c
@@ -8,10 +8,42 @@
 
 //includes
 #include <stdio.h>
+#include <signal.h>
+#include <sys/time.h>
 #include <curses.h>
 
 //global declarations
+int row, col = 0; //current row, col
+int numRows, numCols; //number of rows, cols
+int msDelay = 1000; //default delay between scrolling
 
+
+void scrollMessage(int signum)
+{
+    signal(SIGALRM, scrollMessage); //reset signal binding jic
+
+    move(row,col);
+    addstr("X");
+    col++;
+    refresh();
+}
+
+//utility copied directly from textbook
+int setTicker( int n_msecs )
+{
+    struct itimerval new_timeset; //creating new struct itimerval to set the timer
+    long n_sec, n_usecs; //for calculations of seconds and microseconds
+
+    n_sec = n_msecs / 1000;
+    n_usecs = ( n_msecs % 1000 ) * 1000L;
+
+    new_timeset.it_interval.tv_sec = n_sec;
+    new_timeset.it_interval.tv_usec = n_usecs;
+    new_timeset.it_value.tv_sec = n_sec;
+    new_timeset.it_value.tv_usec = n_usecs;
+
+    return setitimer(ITIMER_REAL, &new_timeset, NULL);
+}
 
 /*****************************************************************************/
 /* Function: main                                                            */
@@ -21,22 +53,30 @@
 /*****************************************************************************/
 int main( int argc, char* argv[] )
 {
-	printf("Test1\n");
-	initscr();
-	printw("Test2\n");
-	int row, col;
-	getmaxyx(stdscr, row, col);
-	printw("Test3\n");
-	printw("rows = %i, columns = %i\n", row, col);
-	printw("Test4\n");
-	getch();
-	endwin();
+    
+    
+    initscr();
+    crmode();
+    noecho();
+    clear();
 
-	// initscr();			/* Start curses mode 		  */
-	// printw("Hello World !!!");	/* Print Hello World		  */
-	// refresh();			/* Print it on to the real screen */
-	// getch();			/* Wait for user input */
-	// endwin();			/* End curses mode		  */
+    getmaxyx(stdscr, numRows, numCols); //can only use after initscr()
+    col = numCols; //start in right corner of window
+    
+    signal(SIGALRM, scrollMessage);
+    if(setTicker(msDelay) == -1)
+    {
+        perror("setTicker");
+    }
+    else
+    {
+        while(1)
+        {
+            pause();
+        }
+    }
+
+    endwin();
 
     return 0;
 }
